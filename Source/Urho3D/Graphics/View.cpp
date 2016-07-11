5c7e0c6a55188ad6b9ebe9772a912fa8fdfaeb53
reattiva <reattiva@gmail.com>
2016-07-11 22:08:17 +0200

    In a quad, set per-command shader parameters only if they have changed.

--- Source/Urho3D/Graphics/View.cpp
+++ Source/Urho3D/Graphics/View.cpp
@@ -1879,9 +1879,13 @@ void View::RenderQuad(RenderPathCommand& command)
     // Set shaders & shader parameters and textures
     graphics_->SetShaders(vs, ps);
 
-    const HashMap<StringHash, Variant>& parameters = command.shaderParameters_;
-    for (HashMap<StringHash, Variant>::ConstIterator k = parameters.Begin(); k != parameters.End(); ++k)
-        graphics_->SetShaderParameter(k->first_, k->second_);
+    if (command.shaderParametersDirty_)
+    {
+        command.shaderParametersDirty_ = false;
+        const HashMap<StringHash, Variant>& parameters = command.shaderParameters_;
+        for (HashMap<StringHash, Variant>::ConstIterator k = parameters.Begin(); k != parameters.End(); ++k)
+            graphics_->SetShaderParameter(k->first_, k->second_);
+    }
 
     SetGlobalShaderParameters();
     SetCameraShaderParameters(camera_, false);
